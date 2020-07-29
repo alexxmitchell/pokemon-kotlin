56bfa9b35b23652d997bd6a301b1f526edd72345
Alexx Mitchell <alexxmitchell25@gmail.com>
2020-07-28 22:30:46 -0400

    added structure for Pokemon data class

--- app/src/main/java/com/example/pokemon_kotlin/models/Pokemon.kt
+++ app/src/main/java/com/example/pokemon_kotlin/models/Pokemon.kt
@@ -7,5 +7,30 @@ import com.squareup.moshi.JsonClass
 data class Pokemon (
     @Json(name = "name") val pokeName: String,
     @Json(name = "weight") val pokeWeight: String,
-    @Json(name = "id") val pokeId: Int
+    @Json(name = "id") val pokeId: Int,
+    @Json(name = "abilities") val pokeAbilities: List<Abilities>,
+    @Json(name = "forms") val pokeForms: List<Forms>,
+    @Json(name ="moves") val pokeMoves: List<Moves>
+)
+data class Abilities (
+    @Json(name = "ability") val ability: List<Ability>
+)
+
+data class Ability (
+    val name: String,
+    val url: String
+)
+
+data class Forms (
+    @Json(name = "name") val name: String,
+    @Json(name = "url") val url: String
+)
+
+data class Moves (
+    @Json(name = "move") val move: List<Move>
+)
+
+data class Move (
+    val name: String,
+    val url: String
 )
